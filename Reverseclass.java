bbdd94d8d18e401a418cbfa2145d890a693ef200
Rajarshi Basak <rajarshi.basak@colorado.edu>
2018-10-28 00:14:55 -0600

    Add files via upload

---
+++ Reverseclass.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of Reverseclass here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Reverseclass {
+    
+    int i = 0;
+    for (i = 0; i<8; i++) {
+        System.out.println(reverse(i));
+    }
+}
